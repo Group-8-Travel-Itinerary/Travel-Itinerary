fa5e1257e50b18156769e39340b2d1ce83e31061
Callum Gourlay <65352339+callumgourlay@users.noreply.github.com>
2024-10-10 21:26:24 +0100

    Added more commentary and extra depth

--- integrations.py
+++ integrations.py
@@ -3,8 +3,19 @@
 import requests
 import json
 
+# Function to get the data from the Google Places API
 def google_places():
     return "WIP"
 
-def chat_gpt():
+# Function to facilitate the chats from the GPT API
+def chat_gpt(type, message):
+    type = ["question", "answer"]
+    return "WIP"
+
+# Function to get data from a flights API to give an idea of the prices
+def flights():
+    return "WIP"
+
+# Function to get the weather data from an API
+def weather():
     return "WIP"

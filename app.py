ff58a4b661d820a419f8011f507f720d34418e8e
Callum Gourlay <65352339+callumgourlay@users.noreply.github.com>
2024-10-10 21:12:20 +0100

    Added Commentary & Base Functions

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+# Description: This file contains the code for the Flask app that will be used to run the web application.
+# Imports the necessary modules and libraries
 from flask import Flask, render_template, request, url_for, flash, redirect, session
 import json
 from bs4 import BeautifulSoup
 import requests
 from datetime import date, datetime
+from integrations import google_places, chat_gpt
 
+# Creates a Flask app
 app = Flask(__name__)
 
+# Route for the index page
 @app.route('/')
 def index():
     # Return the index page
---
+++ integrations.py
@@ -0,0 +1,10 @@
+# Description: This file contains the integrations with external APIs
+# Imports the necessary modules and libraries
+import requests
+import json
+
+def google_places():
+    return "WIP"
+
+def chat_gpt():
+    return "WIP"
